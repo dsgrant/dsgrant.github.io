149942bc6171559585cc52a6632e64e43e34c657
dannyg <dannyg@reactive.com>
2014-02-11 13:51:26 -0500

    now with zen

---
+++ js/main.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function(){
+    request = new XMLHttpRequest
+    request.open('GET', 'https://api.github.com/zen', true);
+    el = document.getElementById('zen');
+
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400){
+        // Success!
+        resp = request.responseText
+        console.log(resp);
+        el.innerHTML = resp;
+      } else {
+        // We reached our target server, but it returned an error
+        el.innerHTML = "Zen is only as good as times when there are no zen. Such as now. Zen limit reached.";
+      }
+    }
+
+    request.onerror = function() {
+      // There was a connection error of some sort
+      el.innerHTML = "Whoa, zen'd out...";
+    }
+
+    request.send()
+});
